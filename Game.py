a0cef927b7398428a246201fcd6183b50a5a99b0
alhart2015 <alhart@davidson.edu>
2018-03-30 15:45:19 -0400

    Clean team nicknames into actual names

--- Game.py
+++ Game.py
@@ -3,6 +3,39 @@ from retrosheet_schema import VISITING_TEAM
 from retrosheet_schema import HOME_TEAM_SCORE
 from retrosheet_schema import VISITING_TEAM_SCORE
 
+TEAM_NICKNAMES = {
+    "ANA" : "Los Angeles Angels",
+    "ARI" : "Arizona Diamondbacks",
+    "ATL" : "Atlanta Braves",
+    "BAL" : "Baltimore Orioles",
+    "BOS" : "Boston Red Sox",
+    "CHA" : "Chicago White Sox",
+    "CHN" : "Chicago Cubs",
+    "CIN" : "Cincinnati Reds",
+    "CLE" : "Cleveland Indians",
+    "COL" : "Colorado Rockies",
+    "DET" : "Detroit Tigers",
+    "HOU" : "Houston Astros",
+    "KCA" : "Kansas City Royals",
+    "LAN" : "Los Angeles Dodgers",
+    "MIA" : "Miami Marlins",
+    "MIL" : "Milwaukee Brewers",
+    "MIN" : "Minnesota Twins",
+    "NYA" : "New York Yankees",
+    "NYN" : "New York Mets",
+    "OAK" : "Oakland A's",
+    "PHI" : "Philadelphia Phillies",
+    "PIT" : "Pittsburgh Pirates",
+    "SDN" : "San Diego Padres",
+    "SEA" : "Seattle Mariners",
+    "SFN" : "San Francisco Giants",
+    "SLN" : "St. Louis Cardinals",
+    "TBA" : "Tampa Bay Rays",
+    "TEX" : "Texas Rangers",
+    "TOR" : "Toronto Blue Jays",
+    "WAS" : "Washington Nationals"
+}
+
 class Game(object):
     """Represents a single game in the season"""
     def __init__(self, home_team, away_team, home_score, away_score):
@@ -12,8 +45,11 @@ class Game(object):
         self.away_score = away_score
 
     def __init__(self, split_row):
-        self.home_team = split_row[HOME_TEAM]
-        self.away_team = split_row[VISITING_TEAM]
+        raw_home_team = split_row[HOME_TEAM]
+        self.home_team = TEAM_NICKNAMES[raw_home_team]
+        raw_away_team = split_row[VISITING_TEAM]
+        self.away_team = TEAM_NICKNAMES[raw_away_team]
+
         self.home_score = split_row[HOME_TEAM_SCORE]
         self.away_score = split_row[VISITING_TEAM_SCORE]
 
--- mlb_ranking_main.py
+++ mlb_ranking_main.py
@@ -14,7 +14,9 @@ def read_game_data(filename):
     with open(filename, 'r') as f:
         for row in f:
             split_row = row.split(",")
-            parsed_game = Game(split_row)
+
+            clean_row = [field.replace('"', '') for field in split_row]
+            parsed_game = Game(clean_row)
 
             all_games.append(parsed_game)
 
@@ -27,14 +29,17 @@ def main():
 
     for game in game_data:
         if game.home_team not in teams:
-            teams[game.home_team] = 1
+            teams[game.home_team] = 0
         if game.away_team not in teams:
-            teams[game.away_team] = 1
+            teams[game.away_team] = 0
+
+        teams[game.home_team] += 1
+        teams[game.away_team] += 1
 
     for team in sorted(teams.keys()):
-        print team
+        print team, teams[team]
 
-    for game in game_data[2]:
+    for game in game_data[:3]:
         print game
 
 
