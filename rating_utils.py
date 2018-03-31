37b5d6441ea106f9c1505889c2159f4cc9d498c3
alhart2015 <alhart@davidson.edu>
2018-03-31 15:48:13 -0400

    Merge two elo implementations into a generic function that can be either

--- rating_utils.py
+++ rating_utils.py
@@ -6,6 +6,7 @@ A collection of all of the ways we'll try and calculate ratings.
 VANILLA_ELO = 0
 SCORE_BASED_ELO = 1
 
+BASE_K_VALUE = 2
 
 '''
 Generic function to update a team's rating based on the specified formula.
@@ -21,9 +22,15 @@ Generic function to update a team's rating based on the specified formula.
 def update(winning_team, losing_team, game, formula):
 
     if formula == VANILLA_ELO:
-        (winner_updated_rating, loser_updated_rating) = vanilla_elo(winning_team, losing_team)
+        (winner_updated_rating, loser_updated_rating) = generic_elo(winning_team, losing_team, BASE_K_VALUE)
     elif formula == SCORE_BASED_ELO:
-        (winner_updated_rating, loser_updated_rating) = score_based_elo(winning_team, losing_team, game)
+        score_diff = game.home_score - game.away_score
+
+        if not game.home_team_won():
+            score_diff *= -1
+
+        k = score_diff if score_diff > 0 else 1
+        (winner_updated_rating, loser_updated_rating) = generic_elo(winning_team, losing_team, k)
     else:
         raise ValueError("Unexpected formula passed to update: {0}".format(formula))
 
@@ -35,41 +42,18 @@ def update(winning_team, losing_team, game, formula):
 '''
 For starters we're gonna go with vanilla elo, as presented by 
 https://metinmediamath.wordpress.com/2013/11/27/how-to-calculate-the-elo-rating-including-example/
-This doesn't factor in things like score difference, but I'll
-implement that later. 
+This takes any K value, which means it can either ignore or
+consider the difference in score. In the version called by the
+VANILLA_ELO constant, it does not consider score. In the
+SCORE_BASED_ELO version, it does.
 
 @param winning_team - the team that won the game
 @param losing_team - the team that lost the game
-@param game - the game in question
+@param k - the k value to use
 
 @return a (winning_team_rating, losing_team_rating) tuple with each team's updated elo
 '''
-def vanilla_elo(winning_team, losing_team):
-
-    k = 2
-
-    winner_transformed_rating = winning_team.transform_rating()
-    loser_transformed_rating = losing_team.transform_rating()
-
-    winner_expected_score = winner_transformed_rating / (winner_transformed_rating + loser_transformed_rating)
-    loser_expected_score = loser_transformed_rating / (winner_transformed_rating + loser_transformed_rating)
-
-    winner_updated_rating = winning_team.rating + k * (1 - winner_expected_score)
-    loser_updated_rating = losing_team.rating + k * (-loser_expected_score)
-
-    return (winner_updated_rating, loser_updated_rating)
-
-'''
-A variation of elo that takes run differential into account.
-'''
-def score_based_elo(winning_team, losing_team, game):
-
-    score_diff = game.home_score - game.away_score
-
-    if not game.home_team_won():
-        score_diff *= -1
-
-    k = score_diff if score_diff > 0 else 1
+def generic_elo(winning_team, losing_team, k):
 
     winner_transformed_rating = winning_team.transform_rating()
     loser_transformed_rating = losing_team.transform_rating()
---
+++ stat_scraper.py
@@ -0,0 +1,6 @@
+'''
+I've always wanted to write something that scraped stuff from the
+internet daily, so here we go. This'll (hopefully) go to mlb.com
+and pull down the data for all games this season. We'll see how it
+goes.
+'''
