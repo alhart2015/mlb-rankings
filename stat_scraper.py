5186b4575355199e55b9ab56ca178141bfc91868
alhart2015 <alhart@davidson.edu>
2018-04-02 22:29:20 -0400

    Clean up stat scraper code

--- stat_scraper.py
+++ stat_scraper.py
@@ -16,23 +16,29 @@ import Game
 
 MLB_API_URL = 'http://gd.mlb.com/components/game/mlb/' \
                 'year_{0}/month_{1:02d}/day_{2:02d}/'
-GAMEDAY_SCOREBOARD = 'scoreboard.xml'
+GAMEDAY_SCOREBOARD_FILENAME = 'scoreboard.xml'
 
-
-test = 'http://gd.mlb.com/components/game/mlb/year_2015/month_03/day_28/master_scoreboard.json'
+TEAM_TAG = 'team'
+NAME_TAG = 'name'
+RUNS_TAG = 'R'
 
 '''
 Add the date you want to scrape to the base mlb url and return the
 url for the league-wide scoreboard for the day.
 '''
 def scoreboard_url_for_date(year, month, day):
-    return MLB_API_URL.format(year, month, day) + GAMEDAY_SCOREBOARD
+    return MLB_API_URL.format(year, month, day) + GAMEDAY_SCOREBOARD_FILENAME
 
-def scrape_mlb(year, month, day):
+def pull_games_for_day(year, month, day):
     api_url = scoreboard_url_for_date(year, month, day)
 
     # Make a request to the mlb GameDay API
     response = requests.get(api_url)
+    # NOTE: If you forget what info is here, print response.content
+    # to see what the xml looks like.
+
+    # TODO: Do this better ^. Document what the xml looks like, either
+    # in code or a note somewhere.
 
     # parse the response into an xml tree
     parsed_scoreboard = ElementTree.fromstring(response.content)
@@ -43,50 +49,29 @@ def scrape_mlb(year, month, day):
         found_home_team = False
         for game_info in game_xml_wrapper:
 
-            if game_info.tag == 'team':
+            if game_info.tag == TEAM_TAG:
                 for team_info in game_info:
-                    # print 'found a team'
-                    # print game_info.tag, game_info.attrib, team_info.tag, team_info.attrib
 
                     if not found_home_team:
-                        home_name = game_info.attrib['name']
-                        home_score = int(team_info.attrib['R'])
+                        home_name = game_info.attrib[NAME_TAG]
+                        home_score = int(team_info.attrib[RUNS_TAG])
                         found_home_team = True
                     else:
-                        away_name = game_info.attrib['name']
-                        away_score = team_info.attrib['R']
-                    # runs = team_info.attrib['R']
-                    print game_info.attrib['name'], team_info.attrib['R']
+                        away_name = game_info.attrib[NAME_TAG]
+                        away_score = team_info.attrib[RUNS_TAG]
 
         home_full_name = Game.MLB_CLUB_NAMES[home_name]
         away_full_name = Game.MLB_CLUB_NAMES[away_name]
         game_obj = Game.Game(home_full_name, away_full_name, home_score, away_score)
-        print game_obj
         games.append(game_obj)
-        print
 
-    print len(games)
-    print games
-
-    # print response.content
-
-    # print parsed_scoreboard.getchildren()[0].getchildren()
-
-    # for game in parsed_scoreboard.getchildren():
-    #     for item in game:
-    #         print item, item.attrib
-
-    # print parsed_scoreboard.getroot()
-    
-    # for something in parsed:
-    #     print something
-    # print root
+    return games
 
 '''
 For testing
 '''
 def main():
-    scrape_mlb(2018, 4, 1)
+    games = pull_games_for_day(2018, 4, 1)
 
 if __name__ == '__main__':
     main()
