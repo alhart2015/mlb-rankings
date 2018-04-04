090daab7268241097abf3cc3afaa51300f953923
alhart2015 <alhart@davidson.edu>
2018-04-04 13:27:45 -0400

    Make team_info sql table

--- database_manager.py
+++ database_manager.py
@@ -36,6 +36,22 @@ INSERT_GAME_STATEMENT = '''INSERT INTO games VALUES(
 )
 '''
 
+CREATE_TEAM_INFO_TABLE = '''CREATE TABLE team_info(
+    team_id         INTEGER PRIMARY KEY,
+    code            TEXT,
+    file_code       TEXT,
+    abbreviation    TEXT,
+    name            TEXT,
+    full_name       TEXT,
+    brief_name      TEXT
+)
+'''
+
+INSERT_TEAM_INFO_STATEMENT = '''INSERT INTO team_info VALUES(
+    ?,?,?,?,?,?,?
+)
+'''
+
 '''
 Add a list of games to the database. If a game_id exists in the table
 already, don't try to add it again. Doubleheaders should already be
@@ -66,6 +82,39 @@ def add_games_to_db(games):
     db.commit()
     db.close()
 
+'''
+Read in the team_info file, make a table that matches it, and fill that
+table with the contents of the file.
+'''
+def create_and_populate_team_info(filename):
+    db = sqlite3.connect(DB_LOCATION)
+    cursor = db.cursor()
+
+    # We should only really run this once, so creating the table here
+    # ensures it'll fail and do nothing if we try to run this again by
+    # mistake
+    print 'Creating team_info table'
+    cursor.execute(CREATE_TEAM_INFO_TABLE)
+
+    num_rows_added = 0
+    print 'Adding rows to team_info'
+    with open(filename, 'r') as f:
+        for row in f:
+            clean_row = row.strip()
+            split_row = clean_row.split('|')
+            # skip the header row
+            if split_row[0] != 'team_id':
+                # convert the team_id to an int
+                split_row[0] = int(split_row[0])
+                # add the row to the database
+                cursor.execute(INSERT_TEAM_INFO_STATEMENT, split_row)
+                num_rows_added += 1
+
+    print 'Finished adding {0} rows'.format(num_rows_added)
+
+    db.commit()
+    db.close()
+
 def main():
     # Creates or opens the file that holds the database
     print 'Connecting to the SQLite database'
@@ -76,8 +125,8 @@ def main():
     # get a cursor object and pass the SQL statements to the cursor 
     # object to execute them. 
     cursor = db.cursor()
-    cursor.execute(CREATE_GAMES_TABLE)
-
+    # cursor.execute(CREATE_GAMES_TABLE)
+    
     #Finally it is necessary to commit the changes. 
     db.commit()
     # When we are done working with the DB we need to close the connection
